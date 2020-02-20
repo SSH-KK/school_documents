df78c76e1b8e1caf26dbb5dece742f0335b8dc63
TUTOR03 <artemss20030906@gmail.com>
2020-02-20 23:19:05 +0500

    Update views.py

--- back/views.py
+++ back/views.py
@@ -8,12 +8,17 @@ from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.authtoken.models import Token
 
+#НИЧЕГО НЕ НАДО
 class CardsListAPI(generics.ListAPIView):
 	queryset = Card.objects.all()
 	permission_classes = [permissions.AllowAny]
 	serializer_class = CardListSerializer
 	filter_backends = [OrderingFilter]
 
+# HEADERS:
+# Authorization: Token (тут сам токен)
+#URL: 
+#api/card/<slug>/delete (slug - имя карточки, в модельке есть)
 @api_view(['DELETE'])
 @permission_classes([permissions.IsAuthenticated])
 def DeleteCardAPIVIew(request, slug):
@@ -24,6 +29,15 @@ def DeleteCardAPIVIew(request, slug):
 	else:
 		return Response(status = status.HTTP_400_BAD_REQUEST)
 
+# HEADERS:
+# Authorization: Token (тут сам токен)
+#Body:
+#title: Название(уникальное)
+#type_num: Семистровки/Семинары
+#class_num: 10Б...
+#image: Собственно картинка
+#URL:
+#api/card/create
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def CreateCardAPIView(request):
@@ -33,11 +47,19 @@ def CreateCardAPIView(request):
 		return Response(status = status.HTTP_201_CREATED)
 	else:
 		return Response(status = status.HTTP_400_BAD_REQUEST)
-
+# HEADERS:
+# Authorization: Token (тут сам токен)
+#URL:
+#api/card/create
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def Logout(request):
 	Token.objects.get(user = request.user).delete()
 	return Response(status = status.HTTP_200_OK)
 
-
+#Функция LOGIN:
+#Body:
+#username: ну понятно что
+#password: тем более понятно
+#URL:
+#api/login
