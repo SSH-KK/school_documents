3c1f2eaa5e4afe2453b20dce6efaddf3bac6eb49
TUTOR03 <artemss20030906@gmail.com>
2020-02-27 00:11:10 +0500

    st

--- back/models.py
+++ back/models.py
@@ -53,7 +53,7 @@ class Card(models.Model):
 	image = models.ImageField(upload_to = get_img_path, blank = False, null = False)
 
 	def __str__(self):
-		return((f'{self.title}_{self.type_num}_{self.group_num}_{self.teacher}_{self.post_date}').encode('utf8'))
+		return self.title.encode('utf-8')
 
 @receiver(post_delete, sender = Card)
 def delete_card_img(sender, instance, **kwargs):
