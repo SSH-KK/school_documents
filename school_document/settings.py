a62f2d9f0ac73be03ebd90d05b68ab5f0ce10e94
TUTOR03 <artemss20030906@gmail.com>
2020-02-26 23:49:35 +0500

    small

--- school_document/settings.py
+++ school_document/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dzdp9_jcgmecpcz9amo=+23*ga1z_i^vuv2cbj7ho(hpn(0w$k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -135,3 +135,4 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
